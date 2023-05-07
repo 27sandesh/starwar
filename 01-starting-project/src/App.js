ab3a95b552ee8641d235e35f45be81ef64296c58
27sandesh <kamblesandesh254@gmail.com>
2023-05-07 16:43:20 +0530

    error handling

--- 01-starting-project/src/App.js
+++ 01-starting-project/src/App.js
@@ -5,18 +5,31 @@ import "./App.css";
 
 function App() {
   const [movies, setmovies] = useState([]);
+  const [isLoading, setisLoading] = useState(false);
+  const [error, seterror] = useState(null);
+
   async function fetchMovieHandler() {
-    const response = await fetch("https://swapi.dev/api/films/");
-    const data = await response.json();
-    const transformedMovies = data.results.map((movieData) => {
-      return {
-        id: movieData.episode_id,
-        title: movieData.title,
-        openingText: movieData.opening_crawl,
-        releaseDate: movieData.release_date, // fixed typo here
-      };
-    });
-    setmovies(transformedMovies);
+    setisLoading(true);
+    seterror(null);
+    try {
+      const response = await fetch("https://swapi.dev/api/films/");
+      const data = await response.json();
+      if (!response.ok) {
+        throw new Error("Smoething went wrong");
+      }
+      const transformedMovies = data.results.map((movieData) => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseDate: movieData.release_date, // fixed typo here
+        };
+      });
+      setmovies(transformedMovies);
+    } catch (error) {
+      seterror(error.message);
+    }
+    setisLoading(false);
   }
 
   return (
@@ -25,7 +38,10 @@ function App() {
         <button onClick={fetchMovieHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isLoading && movies.length == 0 && !error && <p>no Movie Found</p>}
+        {isLoading && <p>...LOADING</p>}
+        {!isLoading && error && <p>{error}</p>}
       </section>
     </React.Fragment>
   );
